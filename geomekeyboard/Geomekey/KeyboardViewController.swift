7fe7d4cf7d585aa4a04d1ba9f97f6ce3b4107390
Devine Lu Linvega <me@m0oo.com>
2014-12-26 22:40:00 -0500

    First draft

--- geomekeyboard/Geomekey/KeyboardViewController.swift
+++ geomekeyboard/Geomekey/KeyboardViewController.swift
@@ -8,42 +8,23 @@
 
 import UIKit
 
-var touchStart:CGPoint?
-let tileSize:CGFloat = 40
-var currentPosition:String?
-var previousPosition:String?
-var letterInsert:String?
-var letterRelease:String?
+var currentLetter:String = "other"
 
-class KeyboardViewController: UIInputViewController {
-	
-	@IBOutlet var row1order1button: UIButton!
-	@IBOutlet var row1order2button: UIButton!
-	@IBOutlet var row1order3button: UIButton!
-	@IBOutlet var row1order4button: UIButton!
-	
+class KeyboardViewController: UIInputViewController
+{
 	@IBOutlet var wrapper: UIView!
     @IBOutlet var nextKeyboardButton: UIButton!
 	var geomekeyView: UIView!
-	
-	@IBOutlet var someButton: UIButton!
 	
     override func updateViewConstraints() {
         super.updateViewConstraints()
         // Add custom view sizing constraints here
     }
 	
-	@IBAction func someBUtton(sender: AnyObject)
-	{
-		var proxy = textDocumentProxy as UITextDocumentProxy
-		proxy.insertText("watwat")
-	}
-	
 	override func touchesBegan(touches: NSSet, withEvent event: UIEvent)
 	{
 		let touchesFix = touches.anyObject() as UITouch
 		var newPoint = touchesFix.locationInView(self.wrapper)
-		touchStart = newPoint
 	}
 	
 	override func touchesMoved(touches: NSSet, withEvent event: UIEvent)
@@ -54,21 +35,12 @@ class KeyboardViewController: UIInputViewController {
 	{
 	}
 	
-	func textSetup()
-	{
-		letterRelease = ""
-		letterInsert = ""
-		
-		previousPosition = ""
-		currentPosition = ""
-	}
 
 	func textInject( character:String)
 	{
 		println("INSERT: \(character)")
 		var proxy = textDocumentProxy as UITextDocumentProxy
 		proxy.insertText(character)
-		textSetup()
 	}
 	
     override func viewDidLoad() {
@@ -95,44 +67,31 @@ class KeyboardViewController: UIInputViewController {
 	
 	func templateStart()
 	{
-		createButton("A", order: 0)
-		createButton("B", order: 1)
-		createButton("C", order: 2)
-		createButton("D", order: 3)
+		var targetLayout:Array = dataSet(currentLetter)
 		
-		createButton("E", order: 4)
-		createButton("F", order: 5)
-		createButton("G", order: 6)
-		createButton("H", order: 7)
-		createButton("I", order: 8)
-		createButton("J", order: 9)
+		var i = 0
 		
-		createButton("K", order: 10)
-		createButton("L", order: 11)
-		createButton("M", order: 12)
-		createButton("N", order: 13)
-		createButton("O", order: 14)
-		createButton("P", order: 15)
-		createButton("Q", order: 16)
-		createButton("R", order: 17)
-		
-		createButton("S", order: 18)
-		createButton("T", order: 19)
-		createButton("U", order: 20)
-		createButton("V", order: 21)
-		
-		createButton("W", order: 22)
-		createButton("X", order: 23)
-		createButton("Y", order: 24)
-		createButton("Z", order: 25)
-		createButton(">", order: 26)
-		createButton("<", order: 27)
-		
+		while( i < 27)
+		{
+			if(i < targetLayout.count){
+				createButton(targetLayout[i], order: i)
+			}
+			
+			i += 1
+		}
 		
 		createButton("!", order: 28) // skip
 		createButton("-", order: 29) // space
 		createButton("=", order: 30) // return
 		createButton("?", order: 31) // change keyboard
+	}
+	
+	func templateErase()
+	{
+		var subViews = self.view.subviews
+		for subview in subViews as [UIView]   {
+			subview.removeFromSuperview()
+		}
 	}
 	
 	func createButton(letter:String, order:Int)
@@ -192,8 +151,28 @@ class KeyboardViewController: UIInputViewController {
 		button.backgroundColor = UIColor.greenColor()
 		button.setTitle(letter, forState: UIControlState.Normal)
 		button.addTarget(self, action: "buttonAction:", forControlEvents: UIControlEvents.TouchUpInside)
+//		button.layer.borderWidth = 1
+//		button.layer.borderColor = UIColor.blackColor().CGColor
 		view.addSubview(button)
 		
+	}
+	
+	@IBAction func buttonAction(sender: AnyObject)
+	{
+		var proxy = textDocumentProxy as UITextDocumentProxy
+		proxy.insertText("")
+		
+		if(sender.tag == 29 ){
+			currentLetter = " "
+		}
+		else{
+			currentLetter = dataSet(currentLetter)[sender.tag]
+		}
+		
+		textInject(currentLetter)
+		
+		templateErase()
+		templateStart()
 	}
 	
 	func loadInterface() {
@@ -235,34 +214,35 @@ class KeyboardViewController: UIInputViewController {
     }
 	
 	
-	func dataSet()
+	func dataSet(target:String) -> Array<String>
 	{
-		let a = ["n","l","t","r","c","s","b","m","p","d","g","e","u","i","v","k","f","y","w","z","h","x","o","q","j","a"]
-		let b = ["l","e","a","i","r","o","u","b","s","y","c","d","t","m","p","h","j","f","v","g","n","w","k","q","z"]
-		let c = ["o","a","h","e","i","r","t","u","l","k","y","c","s","n","q","z","m","d","w","p","b","f"]
-		let d = ["e","i","o","a","r","u","l","y","d","n","g","s","m","w","h","f","b","v","t","j","p","c","k","z","q"]
-		let e = ["r","n","s","d","l","t","a","c","m","p","x","e","o","u","i","v","f","g","b","w","y","q","h","k","z","j"]
-		let f = ["o","i","e","l","u","a","r","f","t","y","s","n","w","h","m","d","c","b","g","j","p","k","v"]
-		let g = ["e","a","r","i","l","o","u","n","y","h","g","m","s","w","t","b","f","d","p","z","k","c","v","j"]
-		let h = ["e","o","i","a","y","u","r","t","l","n","m","w","b","f","s","p","h","c","d","g","k","v","q","z"]
-		let i = ["n","c","s","t","o","a","d","l","m","z","v","f","p","r","g","e","b","k","u","i","x","q","h","j","w","y"]
-		let j = ["u","a","o","e","i","r","h","y","n","d","l","m","p","b","t"]
-		let k = ["e","i","a","o","l","n","u","y","h","s","r","w","b","t","f","k","m","d","p","j","c","g","v","z"]
-		let l = ["e","i","a","y","l","o","u","t","d","p","m","v","c","s","n","f","k","g","b","w","h","r","z","x","j","q"]
-		let m = ["a","e","i","o","p","u","y","b","m","n","s","l","f","r","d","w","c","t","h","v","k","g","j","z","q"]
-		let n = ["e","t","o","i","g","a","d","c","s","n","u","y","f","k","v","l","p","r","m","z","h","b","j","w","q","x"]
-		let o = ["n","r","u","p","s","m","l","t","c","g","v","d","i","o","b","w","a","f","e","x","k","h","y","z","q","j"]
-		let p = ["r","e","h","a","o","i","l","t","u","s","y","p","n","m","w","f","b","c","k","g","d","j","v","q"]
-		let q = ["u","e","o","a","r","q","i"]
-		let r = ["e","a","i","o","t","y","m","s","c","d","r","u","n","p","b","g","h","l","v","k","f","w","j","q","z","x"]
-		let s = ["t","s","e","i","u","c","h","p","a","o","m","l","y","n","k","w","q","f","b","r","g","d","v","j","z"]
-		let t = ["e","i","r","a","o","h","y","u","t","l","c","w","s","m","f","n","b","p","g","z","d","k","v","j","q"]
-		let u = ["n","s","l","r","m","t","c","i","a","p","e","d","g","b","o","f","v","k","x","z","y","j","h","q","u","w"]
-		let v = ["e","i","a","o","u","y","r","v","l","s","c","k","n","g"]
-		let w = ["a","i","o","e","h","r","n","l","s","d","k","y","b","t","u","m","f","w","p","c","g","q","v","z"]
-		let x = ["i","a","e","y","o","t","u","c","p","l","b","h","w","f","m","s","d","g","n","q","r","k"]
-		let y = ["l","s","t","a","c","p","m","o","n","e","r","i","g","d","w","b","h","u","f","z","x","k","q","y","v"]
-		let z = ["e","a","o","i","y","z","u","l","w","d","r","m","c","h","k","s","n","g","t","p","b","f","v"]
+		if target == "a" { return ["n","l","t","r","c","s","b","m","p","d","g","e","u","i","v","k","f","y","w","z","h","x","o","q","j","a"] }
+		else if target == "b" { return ["l","e","a","i","r","o","u","b","s","y","c","d","t","m","p","h","j","f","v","g","n","w","k","q","z"] }
+		else if target == "c" { return ["o","a","h","e","i","r","t","u","l","k","y","c","s","n","q","z","m","d","w","p","b","f"] }
+		else if target == "d" { return ["e","i","o","a","r","u","l","y","d","n","g","s","m","w","h","f","b","v","t","j","p","c","k","z","q"] }
+		else if target == "e" { return ["r","n","s","d","l","t","a","c","m","p","x","e","o","u","i","v","f","g","b","w","y","q","h","k","z","j"] }
+		else if target == "f" { return ["o","i","e","l","u","a","r","f","t","y","s","n","w","h","m","d","c","b","g","j","p","k","v"] }
+		else if target == "g" { return ["e","a","r","i","l","o","u","n","y","h","g","m","s","w","t","b","f","d","p","z","k","c","v","j"] }
+		else if target == "h" { return ["e","o","i","a","y","u","r","t","l","n","m","w","b","f","s","p","h","c","d","g","k","v","q","z"] }
+		else if target == "i" { return ["n","c","s","t","o","a","d","l","m","z","v","f","p","r","g","e","b","k","u","i","x","q","h","j","w","y"] }
+		else if target == "j" { return ["u","a","o","e","i","r","h","y","n","d","l","m","p","b","t"] }
+		else if target == "k" { return ["e","i","a","o","l","n","u","y","h","s","r","w","b","t","f","k","m","d","p","j","c","g","v","z"] }
+		else if target == "l" { return ["e","i","a","y","l","o","u","t","d","p","m","v","c","s","n","f","k","g","b","w","h","r","z","x","j","q"] }
+		else if target == "m" { return ["a","e","i","o","p","u","y","b","m","n","s","l","f","r","d","w","c","t","h","v","k","g","j","z","q"] }
+		else if target == "n" { return ["e","t","o","i","g","a","d","c","s","n","u","y","f","k","v","l","p","r","m","z","h","b","j","w","q","x"] }
+		else if target == "o" { return ["n","r","u","p","s","m","l","t","c","g","v","d","i","o","b","w","a","f","e","x","k","h","y","z","q","j"] }
+		else if target == "p" { return ["r","e","h","a","o","i","l","t","u","s","y","p","n","m","w","f","b","c","k","g","d","j","v","q"] }
+		else if target == "q" { return ["u","e","o","a","r","q","i"] }
+		else if target == "r" { return ["e","a","i","o","t","y","m","s","c","d","r","u","n","p","b","g","h","l","v","k","f","w","j","q","z","x"] }
+		else if target == "s" { return ["t","s","e","i","u","c","h","p","a","o","m","l","y","n","k","w","q","f","b","r","g","d","v","j","z"] }
+		else if target == "t" { return ["e","i","r","a","o","h","y","u","t","l","c","w","s","m","f","n","b","p","g","z","d","k","v","j","q"] }
+		else if target == "u" { return ["n","s","l","r","m","t","c","i","a","p","e","d","g","b","o","f","v","k","x","z","y","j","h","q","u","w"] }
+		else if target == "v" { return ["e","i","a","o","u","y","r","v","l","s","c","k","n","g"] }
+		else if target == "w" { return ["a","i","o","e","h","r","n","l","s","d","k","y","b","t","u","m","f","w","p","c","g","q","v","z"] }
+		else if target == "x" { return ["i","a","e","y","o","t","u","c","p","l","b","h","w","f","m","s","d","g","n","q","r","k"] }
+		else if target == "y" { return ["l","s","t","a","c","p","m","o","n","e","r","i","g","d","w","b","h","u","f","z","x","k","q","y","v"] }
+		else if target == "z" { return ["e","a","o","i","y","z","u","l","w","d","r","m","c","h","k","s","n","g","t","p","b","f","v"] }
+		else { return ["e","t","a","o","i","n","s","h","r","d","l","c","u","m","w","f","g","y","p","b","v","k","j","x","q","z"] }
 
 	}
 
